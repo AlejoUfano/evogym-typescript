1ea40bdfe03282d55d5e0d290affab24dc20264e
Alejo Ufano <ufanoalejo@gmail.com>
2023-01-23 17:05:34 -0300

    Feat(NavBar) NavBar component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import NavBar from '@/scenes/navbar'
+import { useEffect, useState } from 'react'
+import { SelectedPage } from '@/shared/types'
+
 function App() {
 
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
+
+  useEffect(()=>{
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true)
+        setSelectedPage(SelectedPage.Home)
+      } 
+      if (window.scrollY !== 0) setIsTopOfPage(false)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  },[])
+
   return (
-    <div className="app">app</div>
+    <div className="app bg-gray-20">
+      <NavBar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+    </div>
   )
 }
 
