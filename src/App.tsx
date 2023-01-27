8313a9fde5047981ecc614bae4f8d30353cb2e4c
Alejo Ufano <ufanoalejo@gmail.com>
2023-01-27 16:54:07 -0300

    Feat(App.tsx) ContactUs component added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import NavBar from '@/scenes/navbar'
 import Home from '@/scenes/home'
 import Benefits from '@/scenes/benefits'
 import OurClasses from '@/scenes/ourclasses'
+import ContactUs from '@/scenes/contactus'
 import { useEffect, useState } from 'react'
 import { SelectedPage } from '@/shared/types'
 
@@ -28,6 +29,7 @@ function App() {
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
     </div>
   )
 }
